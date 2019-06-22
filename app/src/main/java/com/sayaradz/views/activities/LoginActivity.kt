38216730c105b671347e96f1cf07d31082fd703a
GattalRaouf <gattal.raouf@gmail.com>
2019-06-22 11:15:56 +0100

    reconfigure the login and authentifcation on another firebase app and fb app

--- app/src/main/java/com/sayaradz/views/activities/LoginActivity.kt
+++ app/src/main/java/com/sayaradz/views/activities/LoginActivity.kt
@@ -19,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.sayaradz.R
 import kotlinx.android.synthetic.main.activity_login.*
@@ -31,20 +32,20 @@ import org.json.JSONException
 
 class LoginActivity : AppCompatActivity() {
 
-    val RC_SIGN_IN: Int = 555
-    lateinit var mGoogleSignInClient: GoogleSignInClient
-    lateinit var mGoogleSignInOptions: GoogleSignInOptions
-    lateinit var progressBar: ProgressBar
-    lateinit var callbackManager: CallbackManager
+    private val rcSignIn: Int = 555
+    private var fb: Int = 1
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var mAuth: FirebaseAuth
 
-
-    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        mAuth = FirebaseAuth.getInstance()
+
         setupUI()
     }
 
@@ -57,20 +58,21 @@ class LoginActivity : AppCompatActivity() {
 
         buttonFacebookLogin.setOnClickListener {
             signInFacebook()
-            val currentUser = firebaseAuth.currentUser
-            Log.i("taggggg", currentUser.toString())
         }
 
+        configureGoogleSignIn()
+
         google_btn.setOnClickListener {
-            configureGoogleSignIn()
             signInGoogle()
             progressBar.visibility = View.VISIBLE
         }
+
     }
 
     // ------------------------------ FACEBOOK LOGIN -------------------------------//
     private fun signInFacebook() {
 
+        fb = 0
         callbackManager = CallbackManager.Factory.create()
 
         buttonFacebookLogin.setReadPermissions("email", "public_profile")
@@ -81,56 +83,51 @@ class LoginActivity : AppCompatActivity() {
                 Log.d("", "facebook:onSuccess:$loginResult")
 
                 handleFacebookAccessToken(loginResult.accessToken)
-                GraphLoginRequest(loginResult.accessToken)
-
-            }
-
-            // Method to access Facebook User Data.
-            protected fun GraphLoginRequest(accessToken: AccessToken) {
-                val graphRequest = GraphRequest.newMeRequest(
-                    accessToken
-                ) { jsonObject, graphResponse ->
-                    try {
-
-                        Log.e("log", jsonObject.toString())
-                        val prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-                        val editor = prefs.edit()
-                        editor.putString("fullName", jsonObject.getString("name"))
-                        editor.putString(
-                            "profilePicLink",
-                            "https://graph.facebook.com/${jsonObject.getString("id")}/picture?type=large"
-                        )
-                        editor.putString("address", jsonObject.getString("email"))
-                        editor.apply()
-
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
-                }
-
-                val bundle = Bundle()
-                bundle.putString(
-                    "fields",
-                    "id,name,link,email,gender,last_name,first_name,locale,timezone,updated_time,verified"
-                )
-                graphRequest.parameters = bundle
-                graphRequest.executeAsync()
+                graphLoginRequest(loginResult.accessToken)
 
             }
 
             override fun onCancel() {
-                Toast.makeText(this@LoginActivity, "facebook:onCancel!", Toast.LENGTH_SHORT).show()
-                Log.d("TAG", "facebook:onCancel")
-                //-------------------------------------------------------------------------------------------//
+                Toast.makeText(this@LoginActivity, "facebook Login annulé", Toast.LENGTH_SHORT).show()
 
             }
 
             override fun onError(error: FacebookException) {
-                Toast.makeText(this@LoginActivity, "facebook:onError!", Toast.LENGTH_SHORT).show()
-                Log.d("TAG", "facebook:onError", error)
-                //-------------------------------------------------------------------------------------------//
+                Toast.makeText(this@LoginActivity, "facebook Login erreur!", Toast.LENGTH_SHORT).show()
             }
         })
+
+    }
+
+    // Method to access Facebook User Data.
+    private fun graphLoginRequest(accessToken: AccessToken) {
+        val graphRequest = GraphRequest.newMeRequest(
+            accessToken
+        ) { jsonObject, _ ->
+            try {
+
+                val prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                val editor = prefs.edit()
+                editor.putString("fullName", jsonObject.getString("name"))
+                editor.putString(
+                    "profilePicLink",
+                    "https://graph.facebook.com/${jsonObject.getString("id")}/picture?type=large"
+                )
+                editor.putString("address", jsonObject.getString("email"))
+                editor.apply()
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+
+        val bundle = Bundle()
+        bundle.putString(
+            "fields",
+            "id,name,link,email,gender,last_name,first_name,locale,timezone,updated_time,verified"
+        )
+        graphRequest.parameters = bundle
+        graphRequest.executeAsync()
 
     }
 
@@ -138,17 +135,17 @@ class LoginActivity : AppCompatActivity() {
         Log.d("tag", "handleFacebookAccessToken:$token")
 
         val credential = FacebookAuthProvider.getCredential(token.token)
-        firebaseAuth.signInWithCredential(credential)
+        mAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    Log.d("TAG", "signInWithCredential:success")
-                    val user = firebaseAuth.currentUser
+
                     startActivity(MainActivity.getLaunchIntent(this))
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("TAG", "signInWithCredential:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
-//                    updateUI(null)
+
                 }
 
             }
@@ -161,78 +158,78 @@ class LoginActivity : AppCompatActivity() {
     //TODO fix google auth
     private fun configureGoogleSignIn() {
 
-        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
     }
 
     private fun signInGoogle() {
-        val signInIntent: Intent = mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        fb = 1
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, rcSignIn)
     }
 
-
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        if (fb == 0) callbackManager.onActivityResult(requestCode, resultCode, data)
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-
-            val currentUser = firebaseAuth.currentUser
-
-
+        if (requestCode == rcSignIn) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-
-            Log.i("taggggg", task.result.toString())
             try {
-
-                progressBar.visibility = View.GONE
                 // Google Sign In was successful, authenticate with Firebase
-                val acct = task.getResult(ApiException::class.java)
-                if (acct != null) {
-                    updateUser(acct)
-                }
-                startActivity(MainActivity.getLaunchIntent(this))
-                firebaseAuthWithGoogle(acct!!)
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
 
             } catch (e: ApiException) {
                 progressBar.visibility = View.GONE
-
                 // Google Sign In failed, update UI appropriately
-                Log.w("Log", "Google sign in failed", e)
+                Log.w("Login", "Google sign in failed", e)
+                Toast.makeText(this, "Auth Failed", Toast.LENGTH_LONG).show()
+                // ...
             }
-        } else {
-            callbackManager.onActivityResult(requestCode, resultCode, data)
-        }
-    }
 
-    private fun updateUser(acct: GoogleSignInAccount) {
-        val prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val editor = prefs.edit()
-
-        if (acct != null) {
-            editor.putString("fullName", acct.displayName)
-            editor.putString("profilePicLink", acct.photoUrl.toString())
-            editor.putString("address", acct.email)
-            editor.apply()
         }
     }
 
     private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("", "firebaseAuthWithGoogle:" + acct.id!!)
 
         val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val user = firebaseAuth.currentUser
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("", "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, "Auth Failed", Toast.LENGTH_LONG).show()
+                    updateUI(null)
+                }
 
-                startActivity(MainActivity.getLaunchIntent(this))
-
-            } else {
-                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+                // ...
             }
+    }
+
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+            val editor = prefs.edit()
+
+            editor.putString("fullName", user.displayName)
+            editor.putString("profilePicLink", user.photoUrl.toString())
+            editor.putString("address", user.email)
+            editor.apply()
+
+            startActivity(MainActivity.getLaunchIntent(this))
+            finish()
         }
     }
 
@@ -240,15 +237,9 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-        if (account != null) {
-            updateUser(account)
-        }
-        val user = FirebaseAuth.getInstance().currentUser
-        if (user != null) {
-            startActivity(MainActivity.getLaunchIntent(this))
-            finish()
-        }
+
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
     }
 
     companion object {
---
+++ app/src/test/java/com/sayaradz/views/activities/BrandsActivityTest.kt
@@ -0,0 +1,12 @@
+package com.sayaradz.views.activities
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BrandsActivityTest {
+
+    @Test
+    fun onStart() {
+    }
+}
