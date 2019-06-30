ef7f7a50e406568dea97d139792241e894a5857a
GattalRaouf <gattal.raouf@gmail.com>
2019-06-30 14:40:20 +0100

    creating the compsing car activity

---
+++ app/src/main/java/com/sayaradz/views/activities/CarComposingActivity.kt
@@ -0,0 +1,13 @@
+package com.sayaradz.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sayaradz.R
+
+class CarComposingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_composing)
+    }
+}
